9405cf15cff72c4ce6a0448c75c810fed4650842
Nouman Ejaz <112774247+Nouman64-cat@users.noreply.github.com>
2023-12-10 02:33:34 +0500

    Create FCFS.cpp
    First Come First Served Scheduling Algorithm code in c++ programming language

---
+++ FCFS.cpp
@@ -0,0 +1,73 @@
+//first come first served scheduling algorithm
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Process {
+    int id; //process ID
+    int arrival; //arrival time
+    int burst; //burst time
+};
+
+void fcfs(vector<Process>& processes){
+    int n = processes.size();
+
+    //sort processes based on arrival time
+    sort(processes.begin(), processes.end(), [](const Process& a, const Process& b) {
+        return a.arrival < b.arrival;
+    });
+
+    int totalWaitTime = 0;
+    int totalTurnAroundTime = 0;
+
+    cout<<"Process\tArrival\tBurst\tWaiting\tTurnaround\n";
+
+    int currentTime = 0;
+
+    for(const Process& p : processes){
+        //waiting time for current process
+        int waitTime = max(0, currentTime - p.arrival);
+
+        //turnaround time for current process
+        int turnaroundTime = waitTime + p.burst;
+
+        //update total waiting and turnaround time
+        totalWaitTime += waitTime;
+        totalTurnAroundTime += turnaroundTime;
+
+        //display results for the current time
+        cout<<p.id<<"\t"<<p.arrival<<"\t"<<p.burst<<"\t"<<waitTime<<"\t\t"<<turnaroundTime<<"\n";
+
+        //update current time
+        currentTime += p.burst;
+    }
+
+    //calculate and display average waiting and turnaround time 
+    double averageWaitingTime = static_cast<double>(totalWaitTime) / n;
+    double averageTurnaroundTime = static_cast<double>(totalTurnAroundTime) / n;
+
+    cout<<"Average waiting time = "<<averageWaitingTime<<"\n";
+    cout<<"Average turnaround time = "<<averageTurnaroundTime<<"\n";
+
+}
+
+int main() {
+    int n;
+    cout<<"Enter number of processes : ";
+    cin>>n;
+
+    vector<Process> processes(n);
+
+    for(int i = 0; i < n; i++){
+        processes[i].id = i + 1;
+        cout<<"Enter arrival time for process "<<i + 1<<" : ";
+        cin>>processes[i].arrival;
+        cout<<"Enter burst time for process "<<i + 1<<" : ";
+        cin>>processes[i].burst;
+    }
+
+    fcfs(processes);
+    return 0;
+}
